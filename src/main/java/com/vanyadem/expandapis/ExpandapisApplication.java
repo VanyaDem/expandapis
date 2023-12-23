2f3cff956c593a3e5b6991f43738d37daae83c83
VanyaDem <educationvanyadem@gmail.com>
2023-12-24 01:14:07 +0200

    initial commit

---
+++ src/main/java/com/vanyadem/expandapis/ExpandapisApplication.java
@@ -0,0 +1,13 @@
+package com.vanyadem.expandapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpandapisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpandapisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vanyadem/expandapis/ExpandapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanyadem.expandapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpandapisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
