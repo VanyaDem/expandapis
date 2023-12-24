faba384d178e373c73f0b41a2c4a30da81fed4f6
VanyaDem <educationvanyadem@gmail.com>
2023-12-24 02:31:33 +0200

    added User entity

---
+++ src/main/java/com/vanyadem/expandapis/entity/User.java
@@ -0,0 +1,23 @@
+package com.vanyadem.expandapis.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+}
