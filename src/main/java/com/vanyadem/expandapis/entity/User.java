abdea74e3b916824b539ffee6c26693df92580f5
VanyaDem <educationvanyadem@gmail.com>
2023-12-24 02:37:16 +0200

    Changed @Data annotation to simple getters and setters

--- src/main/java/com/vanyadem/expandapis/entity/User.java
+++ src/main/java/com/vanyadem/expandapis/entity/User.java
@@ -1,11 +1,11 @@
 package com.vanyadem.expandapis.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Entity
-@Data
 @NoArgsConstructor
 @Table(name = "users")
 public class User {
@@ -20,4 +20,45 @@ public class User {
 
     @Column(name = "password",nullable = false)
     private String password;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(id, user.id)) return false;
+        if (!Objects.equals(username, user.username)) return false;
+        return Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
 }
