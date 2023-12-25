59e2a3bc3dc5f7aa00faeec78e77ace25beb04ed
VanyaDem <educationvanyadem@gmail.com>
2023-12-25 14:31:31 +0200

    added custom exception

---
+++ src/main/java/com/vanyadem/expandapis/exceptions/SuchUserExistException.java
@@ -0,0 +1,8 @@
+package com.vanyadem.expandapis.exceptions;
+
+public class SuchUserExistException extends RuntimeException{
+
+    public SuchUserExistException(String message) {
+        super(message);
+    }
+}
