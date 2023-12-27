3572ff69281f5c55460ddaa3947f77d2726db0de
VanyaDem <educationvanyadem@gmail.com>
2023-12-27 16:53:34 +0200

    added exception handling for BadSqlGrammarException

--- src/main/java/com/vanyadem/expandapis/exceptions/AppExceptionHandler.java
+++ src/main/java/com/vanyadem/expandapis/exceptions/AppExceptionHandler.java
@@ -3,6 +3,7 @@ package com.vanyadem.expandapis.exceptions;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,14 +15,23 @@ public class AppExceptionHandler {
         AppError error = AppError.of(400, exception.getMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler
     public ResponseEntity<AppError> handleValidationException(ValidationException exception) {
         AppError error = AppError.of(400, exception.getMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler
     public ResponseEntity<AppError> handleSuchTableExistException(SuchTableExistException exception) {
         AppError error = AppError.of(400, exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<AppError> handleSuchBadSqlGrammarException(BadSqlGrammarException exception) {
+        AppError error = AppError.of(400,
+                "Incorrect number or type of columns! Each record must have the same number and type of columns");
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
